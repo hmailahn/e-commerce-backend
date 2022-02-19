6731340a70bbfa97701711b146b6988e01e6c8ba
Heidi Mailahn <92544035+hmailahn@users.noreply.github.com>
2022-02-19 13:42:07 -0600

    added code to product, category, product id, and tag models'

--- models/Category.js
+++ models/Category.js
@@ -6,9 +6,17 @@ class Category extends Model {}
 
 Category.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
     // define columns
   },
-  {
     sequelize,
     timestamps: false,
     freezeTableName: true,
